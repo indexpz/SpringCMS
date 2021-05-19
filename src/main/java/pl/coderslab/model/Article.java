7894c7b66a96b38246bdfb98cdaf2650735da980
Paweł Zieliński <pawelz72@gmail.com>
2021-05-19 18:52:32 +0200

    zadanie 3

---
+++ src/main/java/pl/coderslab/model/Article.java
@@ -0,0 +1,93 @@
+package pl.coderslab.model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 200, nullable = false)
+    private String title;
+    @Column(length = 100, nullable = false)
+    @OneToOne(optional = false)
+    @JoinColumn(name = "author_id")
+    private Author author;
+    @Column
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Category> categories;
+    @Column(length = 255)
+    private String content;
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+    @Column(name = "updated_on")
+    private LocalDateTime updatedOn;
+    @PrePersist
+    public void prePersist() {
+        createdOn = LocalDateTime.now();
+    }
+    @PreUpdate
+    public void preUpdate() {
+        updatedOn = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
